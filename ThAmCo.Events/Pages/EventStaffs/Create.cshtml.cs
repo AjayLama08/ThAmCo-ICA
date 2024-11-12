4bb287df03122066c2f552fdab8795d82341fb14
Ajay Lama <q2043246@live.tees.ac.uk>
2024-11-12 21:45:26 +0000

    Working on CRUD for Events

---
+++ ThAmCo.Events/Pages/EventStaffs/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ThAmCo.Events.Data;
+
+namespace ThAmCo.Events.Pages.EventStaffs
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ThAmCo.Events.Data.EventsDbContext _context;
+
+        public CreateModel(ThAmCo.Events.Data.EventsDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["EventId"] = new SelectList(_context.Events, "EventId", "Title");
+        ViewData["StaffId"] = new SelectList(_context.Staffs, "StaffId", "FirstName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Staffing Staffing { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Staffings.Add(Staffing);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
