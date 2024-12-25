35d094b9122b5c6569317e4a2e80c867de10b0b8
Ajay Lama <q2043246@live.tees.ac.uk>
2024-12-25 19:50:21 +0000

    All completed

--- ThAmCo.Events/Pages/Venues/Index.cshtml.cs
+++ ThAmCo.Events/Pages/Venues/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using NuGet.Frameworks;
 using ThAmCo.Events.Data;
@@ -12,15 +13,23 @@ namespace ThAmCo.Events.Pages.Venues
     public class IndexModel : PageModel
     {
         private readonly EventsDbContext _context;
+        private readonly EventTypeService _eventTypeService;
         private readonly AvailabilityService _availabilityService;
         private readonly VenueService _venueService;
 
-        public IndexModel(EventsDbContext context, AvailabilityService availabilityService, VenueService venueService)
+        public IndexModel(EventsDbContext context,EventTypeService eventTypeService, AvailabilityService availabilityService, VenueService venueService)
         {
             _context = context;
+            _eventTypeService = eventTypeService;
             _availabilityService = availabilityService;
             _venueService = venueService;
         }
+
+        // Empty SelectListItem List for event type drop down
+        public List<SelectListItem> EventTypeItems { get; set; } = [];
+
+        [BindProperty]
+        public Event Event { get; set; } = default!;
 
         [BindProperty]
         public List<VenueListItem> AvailableVenues { get; set; }
@@ -38,16 +47,16 @@ namespace ThAmCo.Events.Pages.Venues
         public DateTime SelectedVenueDate { get; set; }
 
         [BindProperty]
-        public string SelectedVenueCode { get; set; }
-
-        [BindProperty]
         public string EventTitle { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
-            BeginDate = new DateTime(2022, 1, 1);
-            EndDate = DateTime.Now.AddMonths(12);
+            BeginDate = new DateTime(2022, 11, 1);
+            EndDate = BeginDate.AddMonths(4);
             EventType = "WED";
+
+            //Popualte event type drop down using service class
+            EventTypeItems = await _eventTypeService.GetEventTypeSelectListAsync(); ;
 
             var availableVenues = await _availabilityService.GetAvailabilityListAsync(BeginDate, EndDate, EventType);
             if (availableVenues != null)
@@ -67,7 +76,12 @@ namespace ThAmCo.Events.Pages.Venues
 
         public async Task<IActionResult> OnPostFilterAsync()
         {
+            // Fetch the event type select list
+            EventTypeItems = await _eventTypeService.GetEventTypeSelectListAsync();
+
+            // Filter available venues based on the selected event type
             var availableVenues = await _availabilityService.GetAvailabilityListAsync(BeginDate, EndDate, EventType);
+
             if (availableVenues != null)
             {
                 AvailableVenues = availableVenues.Select(v => new VenueListItem
